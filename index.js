e0905176ef4280ce049444b9a063c99dd104d5c4
Asif Nawaz <asif.nawaz@centralnic.com>
2024-05-03 10:56:12 +0100

    feat(lib files): refactored semantic-release notify plugin structure to align with semantic release

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-import verifyConditions from './lib/verifyConditions.js';
-import success from './lib/success.js';
+import verifyNotification from './lib/verifyConditions.js';
+import successNotify from './lib/success.js';
 
-export { verifyConditions, success };
+let verified;
+
+export async function verifyConditions(pluginConfig, context) {
+  await verifyNotification(pluginConfig, context);
+  verified = true;
+}
+
+export async function success(pluginConfig, context) {
+  if (!verified) {
+    await verifyNotification(pluginConfig, context);
+    verified = true;
+  }
+
+  return successNotify(pluginConfig, context);
+}
